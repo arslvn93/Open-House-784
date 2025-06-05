5cfe23a85ac654e6c439fa022dd5ea67b32f1739
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-06-05 16:24:00 -0400

    Update Site

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const config = {
     "streetAddress": "72 Hillcrest Avenue",
     "city": "Brantford",
     "cityStateZip": "Brantford, ON N3T 5X8",
-    "heroSubtitle": "It is a perfect family home, completely turnkey, featuring 4 beds, 3 baths and a pool. Close to amenities, great schools, and trails. Set in a friendly, quiet neighbourhood. Register to receive the Home & Neighbourhood Info Package, virtual tour, floor plan, and comparables.",
-    "backgroundImageUrl": "https://api.typeform.com/responses/files/014b4737190504021807a066568017138427d9a9a90a48daf798b73d4fab8bcf/RRG_Grey_CMYK__1_.png",
+    "heroSubtitle": "It is a perfect family home, completely turnkey, featuring 4 beds, 3 baths and a pool. Close to amenities, great schools, and trails. Set in a friendly, quiet neighbourhood. Register to receive the Home & Neighbourhood Info Package including: Property's High Quality Pictures, Exclusive Video Tour, and Market's Comparables.",
+    "backgroundImageUrl": "https://photos.zillowstatic.com/fp/0c7e6e260c8e23424ab7a640fbc5a14d-cc_ft_1536.webp",
     "listingPrice": "$799,900",
     "numBeds": "4 beds",
     "numBaths": "3 baths",
@@ -21,13 +21,13 @@ const config = {
   "realtor": {
     "fullName": "Ashley Veldkamp",
     "title": "REALTOR®",
-    "phone": null,
+    "phone": "",
     "email": "info@theriserealtygroup.ca",
     "bio": "Rise Realty Group was established with one core principle in mind: to provide the best real estate experience possible. This philosophy inspired our tagline, ‘raising expectations,’ and our name, Rise Realty Group. We bring over 20 years of experience in the real estate industry and a deep commitment to our community. Our mission goes beyond simply facilitating the buying and selling of homes. We are dedicated to helping you make informed decisions, providing clarity and guidance even in the most complex situations. At Rise Realty Group, we understand that real estate is not just about transactions; it’s about building relationships and fostering trust. We strive to redefine what real estate should be, setting a higher standard for service and client satisfaction. At Rise Realty Group, we are here to raise your expectations and deliver exceptional results.",
     "photoUrl": "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/22c01a4b-310d-4097-a0f7-5e664f5b7ed5/028A4024.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB466XHUXCDDF%2F20250604%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250604T180920Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEFoaCXVzLXdlc3QtMiJHMEUCIBYTqf32SemRjAG78HhJzG0%2Fffgos0bRLypjx6Vbr4HTAiEArxR503r%2B6MF55h%2FxE7GguBC77%2B3uSsYLJfEoRFF%2FG9oq%2FwMIMxAAGgw2Mzc0MjMxODM4MDUiDHUqk26jm%2B5k2aZc8SrcA0tgPCE7%2Bm4Wy5hkp8VfXX47xLphgpIC1Gr5VzKQexseUohLQl5uC1q9unERw4t%2FVaK%2FloZwLu%2BoopmKU2lzyuE7ft27v5JEKq7S5GTzdi7uFPYyowT%2BfPVKxgchDXJGKin83c7ZgW3xR0XKUbylm8MLKKK82k6VhzTUKrCJ%2FxxtPOaF7WJxvzLnH0zVuJwmeWLq7buWhfFw1uhC8o55f5Zun4v2ZpxsKReex9ScnW4R8fCIQOLfnYws%2By3GN%2BUlLOTRyYEEzlJ6agJ8JM0vT9OkBb%2FYvRT59cbIwmAaKt3T7M8NZfFaouIZib7zDuJP0GEge9TpPURbvuRdNEYlOL%2BhBSbm%2BlqvACU8LOPvrLyK%2BD1WLmsAGJyNsFg5YO6qZUf7k1oHKUQuG82XXVr5r%2B9S%2FqwXV733iFLD0IIeUZ3SW4OMTWm7Ckb7MIgKHq4Jz2yLTff8eoXHWYaeF1UUTJ3Mybca74uwyDloLONdk4U0Ji8%2BZgH0l63%2BJxMCZm9ibEMdUIWg1ZXggpi%2BOpV0EXZYTGG3IA05QvswAqs2orzjhQcL1TLYaJNauRdKIHjocs0N8WM%2BZDtj9itudIQieyD26ENmlQILuefo22THJUsqV7SieCYJxWWB8qv8MJOHgsIGOqUBWo834a6VWMZYB6QOxXddQoxkD6Ge4DuBylJdJ4Aj2xECBkzguXg45MU8r1zypTD9nGqEbchuvTg4uuh5koaz1InFahgFiXQZSaK5DIJSRJ5iMp22vNf8PA9nK%2BIKTUlCbD8UmwN6GoRSy5NyiboBOTHOj9zfpHjovZ7%2FK1SFTtpAjcmC3adySwxXp9rhrocoXa9uFQ3V9sv7rPam7zqOq7p3h4Dv&X-Amz-Signature=bb6005fb6a7a082bd76850080e59ed73fabfb727fa657db424b283dcbe5580c4&X-Amz-SignedHeaders=host&x-id=GetObject",
     "social": {
       "facebook": "https://www.facebook.com/theriserealtygroup/",
-      "instagram": "ashleyveldkamp"
+      "instagram": "https://www.instagram.com/theriserealtygroup/?hl=en"
     }
   },
   "brokerage": {
@@ -82,36 +82,72 @@ const config = {
       "id": "agent_status",
       "questionText": "Are you currently exploring homes with a real estate agent?",
       "options": [
-        { "value": "committed", "text": "Yes, I'm committed to an agent." },
-        { "value": "not_committed", "text": "No, I'm not committed to an agent." },
-        { "value": "gathering_info", "text": "Just gathering information at this stage." }
+        {
+          "value": "committed",
+          "text": "Yes, I'm committed to an agent."
+        },
+        {
+          "value": "not_committed",
+          "text": "No, I'm not committed to an agent."
+        },
+        {
+          "value": "gathering_info",
+          "text": "Just gathering information at this stage."
+        }
       ]
     },
     {
       "id": "interest_level",
       "questionText": "How interested are you in properties like this?",
       "options": [
-        { "value": "very_interested", "text": "Very interested, actively looking." },
-        { "value": "somewhat_interested", "text": "Somewhat interested, keeping options open." },
-        { "value": "just_browsing", "text": "Just browsing for now." }
+        {
+          "value": "very_interested",
+          "text": "Very interested, actively looking."
+        },
+        {
+          "value": "somewhat_interested",
+          "text": "Somewhat interested, keeping options open."
+        },
+        {
+          "value": "just_browsing",
+          "text": "Just browsing for now."
+        }
       ]
     },
     {
       "id": "neighborhood_engagement",
       "questionText": "How do you usually enjoy the neighborhood?",
       "options": [
-        { "value": "resident_explorer", "text": "I live here and love exploring locally." },
-        { "value": "considering_move", "text": "I'm considering moving to this area." },
-        { "value": "just_visiting", "text": "Just visiting for the open house." }
+        {
+          "value": "resident_explorer",
+          "text": "I live here and love exploring locally."
+        },
+        {
+          "value": "considering_move",
+          "text": "I'm considering moving to this area."
+        },
+        {
+          "value": "just_visiting",
+          "text": "Just visiting for the open house."
+        }
       ]
     },
     {
       "id": "feature_attraction",
       "questionText": "What feature attracted you to this home?",
       "options": [
-        { "value": "design_layout", "text": "The design and layout." },
-        { "value": "location_neighborhood", "text": "The location and neighborhood." },
-        { "value": "home_price", "text": "The price of the home." }
+        {
+          "value": "design_layout",
+          "text": "The design and layout."
+        },
+        {
+          "value": "location_neighborhood",
+          "text": "The location and neighborhood."
+        },
+        {
+          "value": "home_price",
+          "text": "The price of the home."
+        }
       ]
     }
   ],
